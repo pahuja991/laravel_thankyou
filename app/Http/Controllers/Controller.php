f1eb3c06d4b45c9ad1b8018b461047658abdb4ff
pahuja991 <rp824@njit.edu>
2018-10-30 18:53:21 -0400

    Fix:Controller error fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Hhttp://127.0.0.1:8000>ttp\Controllers;
+namespace App\Http\Controllers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
